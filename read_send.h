c6bd9df9223221c937e1e736e1fe9a58b0ac019f
smalnote <smalnote@gmail.com>
2024-12-10 00:27:32 +0800

    zero_copy: read file and send

---
+++ read_send.h
@@ -0,0 +1,6 @@
+#ifndef READ_SEND_H
+#define READ_SEND_H
+
+int read_send(const char *file_path, const char *server_ip, int server_port);
+
+#endif // !READ_SEND_H
---
+++ zero_copy.c
@@ -0,0 +1,48 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "read_send.h"
+
+#define BUFFER_SIZE 4096
+
+typedef int (*SendFunc)(const char *file_path, const char *server_ip,
+                        int server_port);
+
+int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    fprintf(stderr,
+            "Usage: %s <method> <file_path> <server_ip> <server_port>\n",
+            argv[0]);
+    return EXIT_FAILURE;
+  }
+  const char *method = argv[1];
+  const char *file_path = argv[2];
+  const char *server_ip = argv[3];
+  int server_port = atoi(argv[4]);
+
+  if (server_port <= 0 || server_port >= (1 << 16)) {
+    fprintf(stderr, "Invalid port number: %d\n", server_port);
+    return EXIT_FAILURE;
+  }
+
+  printf("Sending file '%s' to '%s:%d'\n", file_path, server_ip, server_port);
+
+  SendFunc send_func = NULL;
+  if (strcmp(method, "read_send") == 0) {
+    send_func = read_send;
+  } else {
+    fprintf(stderr, "Method '%s' not supported\n", method);
+    return EXIT_FAILURE;
+  }
+
+  if (send_func(file_path, server_ip, server_port) == 0) {
+    printf("File sent successfully to %s:%d\n", server_ip, server_port);
+  } else {
+    printf("File sending failed.\n");
+  }
+  return EXIT_SUCCESS;
+}
