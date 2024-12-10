f9b7cb5dd915bcee0d3a2ca73a78522ba4ddf0cf
Note Smal <smalnote@gmail.com>
2024-12-10 13:34:49 +0800

    zero_copy: memory-map file (#1)

---
+++ mmap_send.h
@@ -0,0 +1,6 @@
+#ifndef MMAP_SEND_H
+#define MMAP_SEND_H
+
+int mmap_send(const char *file_path, const char *server_ip, int server_port);
+
+#endif // !MMAP_SEND_H
--- zero_copy.c
+++ zero_copy.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "mmap_send.h"
 #include "read_send.h"
 
 #define BUFFER_SIZE 4096
@@ -34,6 +35,8 @@ int main(int argc, char *argv[]) {
   SendFunc send_func = NULL;
   if (strcmp(method, "read_send") == 0) {
     send_func = read_send;
+  } else if (strcmp(method, "mmap_send") == 0) {
+    send_func = mmap_send;
   } else {
     fprintf(stderr, "Method '%s' not supported\n", method);
     return EXIT_FAILURE;
